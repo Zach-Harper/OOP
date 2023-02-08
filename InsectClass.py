9a5f4d9e273bba8aedad080d088b0029c68b755f
Zach Harper <zach_harper3@gmail.com>
2023-02-08 14:13:26 -0600

    Class

---
+++ InsectClass.py
@@ -0,0 +1,16 @@
+import random
+
+class Insect:
+    def __init__(self):
+        self.wings = 2
+
+        self.legs = 4
+
+        self.fly = range(1,10)
+
+    def flight(self):
+        if random.randint() == range(1,10):
+            
+        
+
+
